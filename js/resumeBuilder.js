90a6feed30e37be74c436405af244b3983809439
Brenton Dill <bdill12@gmail.com>
2015-02-25 10:14:41 -0800

    More

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -21,9 +21,9 @@ var bio = {
 		var formattedLocation = HTMLlocation.replace( "%data%", this.contacts.location );
 		var formattedWelcomeMessage = HTMLWelcomeMsg.replace( "%data%", this.welcomeMessage);
 		var formattedBiopic = HTMLbioPic.replace( "%data%", this.url );
+		$("#header").prepend(formattedBiopic);
 		$("#header").prepend(formattedRole);
 		$("#header").prepend(formattedName);
-		$("#header").append(formattedBiopic);
 		$("#header").append(formattedWelcomeMessage);
 		$("#topContacts").append(formattedMobile);
 		$("#footerContacts").append(formattedMobile);
