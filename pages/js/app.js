851fb3cbd5e3f193e763c833ff67f39261a64d83
Brenton Dill <bdill12@gmail.com>
2015-03-21 08:00:00 -0700

    minor adjustments
    Made the speeds of the enemies varied

--- pages/js/app.js
+++ pages/js/app.js
@@ -7,6 +7,7 @@ var Enemy = function(x, y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = Math.ceil(Math.random() * 505);
     this.y = Math.floor(Math.random() * 3) * 83;
+    this.speed = Math.floor(Math.random() * 1200);
 };
 
 // Update the enemy's position, required method for game
@@ -15,10 +16,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + ((Math.random() * 1000) * dt);
+    this.x = this.x + (this.speed * dt);
     if (this.x > 505) {
         this.x = 0;
         this.y = Math.floor(Math.random() * 3) * 83;
+        this.speed = Math.floor(Math.random() * 1200);
     }
 
     function collision(playx, bugx) {
