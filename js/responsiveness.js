1bfa1ff358171e687da56a2e45797c11dc638be1
Brenton Dill <bdill12@gmail.com>
2015-04-11 10:38:29 -0700

    respond to me

--- js/responsiveness.js
+++ js/responsiveness.js
@@ -5,10 +5,8 @@ window.onload = function() {
 var resize = function() {
 	var width = $(document).width();
 	if (width <= 480) {
-		$('#experience').after($('#experience-caption'));
-		$('#education').after($('#education-caption'));
-		$('#writing').after($('#writing-caption'));
-		$('#webdev').after($('#webdev-caption'));
+		oneColumn();
+		removeIt();
 	}
 };
 
@@ -20,17 +18,40 @@ var reload = function() {
 	var width = $(document).width();
 	console.log(width);
 	if (width <= 480) {
-		$('#experience').after($('#experience-caption'));
-		$('#education').after($('#education-caption'));
-		$('#writing').after($('#writing-caption'));
-		$('#webdev').after($('#webdev-caption'));
+		oneColumn();
+		removeIt();
 	}
-	if (width >= 480 && width <= 600) {
-		$('#experience').after($('#education'));
-		$('#education').after($('#experience-caption'));
+	if (width >= 480) {
+		$('#experience').after($('#edupic'));
+		$('#edupic').after($('#experience-caption'));
 		$('#experience-caption').after($('#education-caption'));
+		$('#writing-caption').after($('#webdev-caption'));
+		$('#webdev-caption').after($('#writing'));
 		$('#writing').after($('#webdev'));
-		$('#webdev').after($('writing-caption'));
-		$('#writing-caption').after($('webdev-caption'));
+		$('#choice1').after($('#instructions'));
+		addIt();
 	}
+};
+
+var removeIt = function(){
+	console.log('well, the function is getting called.');
+	$('#experience').removeClass( "sb-toggle-left" );
+	$('#edupic').removeClass( "sb-toggle-right" );
+	$('#writing').removeClass( "sb-toggle-left2" );
+	$('#webdev').removeClass( "sb-toggle-right2" );
+};
+
+var addIt = function() {
+	$('#experience').addClass("sb-toggle-left");
+	$('#edupic').addClass("sb-toggle-right");
+	$('#writing').addClass("sb-toggle-left2");
+	$('#webdev').addClass("sb-toggle-right2");
+};
+
+var oneColumn = function(){
+	$('#experience').after($('#experience-caption'));
+	$('#edupic').after($('#education-caption'));
+	$('#writing').after($('#writing-caption'));
+	$('#webdev').after($('#webdev-caption'));
+	$('#name').after($('#instructions'));
 };
