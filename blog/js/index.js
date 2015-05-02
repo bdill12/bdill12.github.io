9377cbefa4fa40a053b952dcccb8830b6545cae6
Brenton Dill <bdill12@gmail.com>
2015-05-02 04:10:50 -0700

    correct

--- blog/js/index.js
+++ blog/js/index.js
@@ -1,6 +1,8 @@
+var array;
 $(function(){
 	$.get("http://bdill12.github.io/blog/posts.xml", function(response){
-		console.log(response());
+		array = response;
+		console.log(response);
 		function AppViewModel() {
 			this.posts = ko.observableArray([response.getElementsByTag('posts')]);
 			ko.applyBindings(new AppViewModel());
