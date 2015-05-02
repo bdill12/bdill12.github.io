33b44f36e0caf868386b6129be1706653075e159
Brenton Dill <bdill12@gmail.com>
2015-05-02 08:52:48 -0700

    k

--- blog/js/index.js
+++ blog/js/index.js
@@ -9,9 +9,11 @@ setTimeout(function(){
 		self.posts = ko.observableArray([]);
 		for (var i = 0; i < data.firstElementChild.children.length; i++) {
 			var myTitle = data.firstElementChild.children[i].children[0].textContent;
-			var myContent = data.firstElementChild.children[i].children[1].textContent;
+			var mySnippet = data.firstElementChild.children[i].children[1].textContent;
 			var myNum = data.firstElementChild.children[i].children[2].textContent;
-			self.posts.push({title: myTitle, content: myContent, postNum: myNum});
+			var myDate = data.firstElementChild.children[i].children[3].textContent;
+			var myContent = data.firstElementChild.children[i].children[4].textContent;
+			self.posts.push({title: myTitle, content: myContent, postNum: myNum, snippet: mySnippet, date: myDate});
 		}
 		console.log("1. " + self.posts()[0]);
 	}
