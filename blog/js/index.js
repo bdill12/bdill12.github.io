fa89028be4ad195dfc7d521400b78234858ce930
Brenton Dill <bdill12@gmail.com>
2015-05-06 10:10:28 -0700

    ok

--- blog/js/index.js
+++ blog/js/index.js
@@ -1,23 +1,26 @@
-$(function(){
-var data = "";
-$.get("http://bdill12.github.io/blog/posts.xml", function(response, textStatus, jqXHR){
-	data = response;
-}, 'xml');
-setTimeout(function(){
-	function AppViewModel() {
-		var self = this;
-		self.posts = ko.observableArray([]);
-		for (var i = 0; i < data.firstElementChild.children.length; i++) {
-			var myTitle = data.firstElementChild.children[i].children[0].textContent;
-			var mySnippet = data.firstElementChild.children[i].children[1].textContent;
-			var myNum = data.firstElementChild.children[i].children[2].textContent;
-			var myDate = data.firstElementChild.children[i].children[3].textContent;
-			var myContent = data.firstElementChild.children[i].children[4].textContent;
-			self.posts.push({title: myTitle, content: myContent, postNum: myNum, snippet: mySnippet, date: myDate});
-		}
-		console.log("1. " + self.posts()[0]);
-	}
-	var viewModel = new AppViewModel();
-	ko.applyBindings(viewModel);
-}, 1000);
+$(function() {
+  var data = "";
+  $.get("http://bdill12.github.io/blog/posts.xml", function(response, textStatus, jqXHR) {
+    data = response;
+  }, 'xml');
+  setTimeout(function() {
+      function AppViewModel() {
+        var self = this;
+        self.posts = ko.observableArray([]);
+        for (var i = 0; i < data.firstElementChild.children.length; i++) {
+          var myTitle = data.firstElementChild.children[i].children[0].textContent;
+          var myNum = data.firstElementChild.children[i].children[2].textContent;
+          var myDate = data.firstElementChild.children[i].children[3].textContent;
+          var myContent = data.firstElementChild.children[i].children[4].textContent;
+          self.posts.push({
+            title: myTitle,
+            content: myContent,
+            postNum: myNum,
+            date: myDate
+          });
+        }
+      };
+    var viewModel = new AppViewModel(); ko.applyBindings(viewModel);
+
+  }, 1000);
 });
--- myers_briggs/js/main.js
+++ myers_briggs/js/main.js
@@ -1,155 +1,154 @@
 var ViewModel = function() {
     // Data
     var self = this;
-    self.types = [
-	{
-		letters: "ISTJ",
-		pros: ["Quiet", "Serious", "Dependable", "Practical", "Matter-of-fact"],
-		cons: ["Take things too seriously at times", "Judgemental", "Very little abstract thinking", "Uncomfortable expressing emotions", "Hard on themselves"],
-		work: ["Things done by the book", "Highly organized", "Clear expectations", "Teamwork should be rare", "Social justice" ],
-		snippets: ["ISTJs keep their feet on the ground and make clear, rational decisions.",],
-		names: ["Natalie Portman", "Herber Hoover", "Hermione Granger", "George Washington", "George H.W. Bush"]
-	},
-	{
-		letters: "ISFJ",
-		pros: ["Supportive", "Friendly", "Responsible", "Conscientious", "Committed"],
-		cons: ["Shy", "Too sensitive", "Repress emotions", "Resist change", "Will work themselves to death"],
-		work: ["Not highly competive", "Lots of encouragement", "Service-oriented", "No conflict among co-workers", "Personal relationships important"],
-		snippets: ["ISFJs need to be needed."],
-		names: ["Queen Elizabeht II", "Robert E. Lee", "Dr. Watson", "Sam from Lord of the Rings", "Halle Berry"]
-	}, 
-	{
-		letters: "INFJ",
-		pros: ["Seek meaning in everythiing", "Insightful", "Persuasive", "Organized", "Decisive"],
-		cons: ["Sensitive", "Closed books to all but a few", "Must always be fighting something", "Tire out easily"],
-		work: ["Meaningful", "Creative", "Lots of independence", "Focus on communication", "Focus on individuals"],
-		snippets: ["When INFJs come to believe that something is important, they pursue that goal with a conviction and energy that can catch even their friends and loved ones off guard."],
-		names: ["Nicole Kidman", "Martin Luther King", "Jimmy Carter", "Nelson Mandela", "Mel Gibson"]
-	},
-	{
-		letters: "INTJ",
-		pros: ["Original and open-minded", "Great drive", "See patterns in external events", "High confidence", "Independent"],
-		cons: ["Arrogant", "Judgmental", "Stuck in their heads", "Don't deal well with structure", "Hopeless in love"],
-		work: ["Prefer to work alone", "High standards", "Meritocracy", "Loosely defined roles", "Lots of challenges"],
-		snippets: ["A paradox to most observers, INTJs are able to live by glaring contradictions that nonetheless make perfect sense."],
-		names: ["Vladimir Putin", "Thomas Jefferson", "John F. Kennedy", "Hannibal", "Gandalf the Grey"]
-	},
-	{
-		letters: "ISTP",
-		pros: ["Tolerant", "Great in a crisis", "Relaxed", "Spontaneous, but rational", "Creative, but practical"],
-		cons: ["Stubborn", "Private and reserved", "Insensitive", "Risky behavior", "Easily bored"],
-		work: ["Need to know how things work", "Hands-on experiences", "Independence", "Practical solutions", "Concrete results" ],
-		snippets: ["Rather than some sort of vision quest, ISTPs are merely exploring the viability of a new interest when they make seismic shifts."],
-		names: ["Clint Eastwood", "Tom Cruise", "Indiana Jones", "Jack Bauer", "Angus MacGuyver" ]
-	},
-	{
-		letters: "ISFP",
-		pros: ["Charming", "Great imaginations", "Sensitive", "Curious", "Passion"],
-		cons: ["Independent to a fault", "High anxiety", "Very competitive", "Love themselves, hate themselves", "Unpredictable"],
-		work: ["Unpredictability", "Short term commitments", "Dynamic, always changing environments", "Freelancing and consulting","Room to improve things"],
-		snippets: ["ISFPs always know just the compliment to soften a heart that's getting ready to call their risks irresponsible or reckless."],
-		names: ["Bob Dylan", "Marie Antoinette", "Millard Filmore", "John Travolta", "Donald Trump"]
-	},
-	{
-		letters: "INFP",
-		pros: ["Idealistic", "loyal", "curious", "maximize potential of others", "adaptable"],
-		cons: ["Other Types Coming Soon"],
-		work: ["Other Types Coming Soon"],
-		snippets: ["Other Types Coming Soon"],
-		names: ["Other Types Coming Soon"]
-	},
-	{
-		letters: "INTP",
-		pros: ["theoretical", "abstract", "quiet", "flexible", "incredible focus"],
-		cons: ["Other Types Coming Soon"],
-		work: ["Other Types Coming Soon"],
-		snippets: ["Other Types Coming Soon"],
-		names: ["Other Types Coming Soon"]
-	},
-	{
-		letters: "ENFP",
-		pros: ["Spontaneous", "Comfortable in unfamiliar situations", "Energetic", "Keen observers", "Brilliantly perceptive"],
-		cons: ["Seek attention", "Act irrationally", "Break rules", "Prone to fantasy", "Can make serious mistakes in judgment"],
-		work: ["Has variety", "Full of Challenges", "Free from tight supervision", "Energetic", "Creative and original"],
-		snippets: ["When people fall in love, they resemble garden-variety ENFPs because ENFPs normally behave like people in love."],
-		names: ["Dr. Suess", "Mark Twain", "Ellen Degeneres", "Walt Disney", "Sandra Bullock"]
-	},
-	{
-		letters: "ESTP",
-		pros: ["flexible", "tolerant", "pragmatic", "energetic", "spontaneous"],
-		cons: ["Other Types Coming Soon"],
-		work: ["Other Types Coming Soon"],
-		snippets: ["Other Types Coming Soon"],
-		names: ["Other Types Coming Soon"]
-	},
-	{
-		letters: "ESFP",
-		pros: ["outgoing", "friendly", "accepting", "exuberant lovers of life", "flexible"],
-		cons: ["Other Types Coming Soon"],
-		work: ["Other Types Coming Soon"],
-		snippets: ["Other Types Coming Soon"],
-		names: ["Other Types Coming Soon"]
-	},
-	{
-		letters: "ENTP",
-		pros: ["quick", "ingenious", "stimulating", "alert", "outspoken"],
-		cons: ["Other Types Coming Soon"],
-		work: ["Other Types Coming Soon"],
-		snippets: ["Other Types Coming Soon"],
-		names: ["Other Types Coming Soon"]
-	},
-	{
-		letters: "ESTJ",
-		pros: ["Practical", "realistic", "matter-of-fact", "decisive", "detail-oriented"],
-		cons: ["Other Types Coming Soon"],
-		work: ["Other Types Coming Soon"],
-		snippets: ["Other Types Coming Soon"],
-		names: ["Other Types Coming Soon"]
-	},
-	{
-		letters: "ESFJ",
-		pros: ["Warmhearted", "conscientious", "cooperative", "work well with others", "dependable"],
-		cons: ["Other Types Coming Soon"],
-		work: ["Other Types Coming Soon"],
-		snippets: ["Other Types Coming Soon"],
-		names: ["Other Types Coming Soon"]
-	},
-	{
-		letters: "ENFJ",
-		pros: ["warm", "empathetic", "responsive", "responsible", "see potential in everyone"],
-		cons: ["Other Types Coming Soon"],
-		work: ["Other Types Coming Soon"],
-		snippets: ["Other Types Coming Soon"],
-		names: ["Other Types Coming Soon"]
-	},
-	{
-		letters: "ENTJ",
-		pros: ["frank", "decisive", "assume leadership readily", "problem solvers", "well-informed"],
-		cons: ["Other Types Coming Soon"],
-		work: ["Other Types Coming Soon"],
-		snippets: ["Other Types Coming Soon"],
-		names: ["Other Types Coming Soon"]
-	}];
-	self.chosenType = ko.observable();
-	self.chosenStrengths = ko.observable();
-	self.chosenWeaknesses = ko.observable();
-	self.chosenWork = ko.observable();
-	self.chosenFamous = ko.observable();
-	self.chosenQuote = ko.observable();
+    self.types = [{
+        letters: "ISTJ",
+        pros: ["Quiet", "Serious", "Dependable", "Practical", "Matter-of-fact"],
+        cons: ["Take things too seriously at times", "Judgemental", "Very little abstract thinking", "Uncomfortable expressing emotions", "Hard on themselves"],
+        work: ["Things done by the book", "Highly organized", "Clear expectations", "Teamwork should be rare", "Social justice"],
+        snippets: ["ISTJs keep their feet on the ground and make clear, rational decisions.", ],
+        names: ["Natalie Portman", "Herber Hoover", "Hermione Granger", "George Washington", "George H.W. Bush"]
+      }, {
+        letters: "ISFJ",
+        pros: ["Supportive", "Friendly", "Responsible", "Conscientious", "Committed"],
+        cons: ["Shy", "Too sensitive", "Repress emotions", "Resist change", "Will work themselves to death"],
+        work: ["Not highly competive", "Lots of encouragement", "Service-oriented", "No conflict among co-workers", "Personal relationships important"],
+        snippets: ["ISFJs need to be needed."],
+        names: ["Queen Elizabeht II", "Robert E. Lee", "Dr. Watson", "Sam from Lord of the Rings", "Halle Berry"]
+      }, {
+        letters: "INFJ",
+        pros: ["Seek meaning in everythiing", "Insightful", "Persuasive", "Organized", "Decisive"],
+        cons: ["Sensitive", "Closed books to all but a few", "Must always be fighting something", "Tire out easily"],
+        work: ["Meaningful", "Creative", "Lots of independence", "Focus on communication", "Focus on individuals"],
+        snippets: ["When INFJs come to believe that something is important, they pursue that goal with a conviction and energy that can catch even their friends and loved ones off guard."],
+        names: ["Nicole Kidman", "Martin Luther King", "Jimmy Carter", "Nelson Mandela", "Mel Gibson"]
+      }, {
+        letters: "INTJ",
+        pros: ["Original and open-minded", "Great drive", "See patterns in external events", "High confidence", "Independent"],
+        cons: ["Arrogant", "Judgmental", "Stuck in their heads", "Don't deal well with structure", "Hopeless in love"],
+        work: ["Prefer to work alone", "High standards", "Meritocracy", "Loosely defined roles", "Lots of challenges"],
+        snippets: ["A paradox to most observers, INTJs are able to live by glaring contradictions that nonetheless make perfect sense."],
+        names: ["Vladimir Putin", "Thomas Jefferson", "John F. Kennedy", "Hannibal", "Gandalf the Grey"]
+      }, {
+        letters: "ISTP",
+        pros: ["Tolerant", "Great in a crisis", "Relaxed", "Spontaneous, but rational", "Creative, but practical"],
+        cons: ["Stubborn", "Private and reserved", "Insensitive", "Risky behavior", "Easily bored"],
+        work: ["Need to know how things work", "Hands-on experiences", "Independence", "Practical solutions", "Concrete results"],
+        snippets: ["Rather than some sort of vision quest, ISTPs are merely exploring the viability of a new interest when they make seismic shifts."],
+        names: ["Clint Eastwood", "Tom Cruise", "Indiana Jones", "Jack Bauer", "Angus MacGuyver"]
+      }, {
+        letters: "ISFP",
+        pros: ["Charming", "Great imaginations", "Sensitive", "Curious", "Passion"],
+        cons: ["Independent to a fault", "High anxiety", "Very competitive", "Love themselves, hate themselves", "Unpredictable"],
+        work: ["Unpredictability", "Short term commitments", "Dynamic, always changing environments", "Freelancing and consulting", "Room to improve things"],
+        snippets: ["ISFPs always know just the compliment to soften a heart that's getting ready to call their risks irresponsible or reckless."],
+        names: ["Bob Dylan", "Marie Antoinette", "Millard Filmore", "John Travolta", "Donald Trump"]
+      }, {
+        letters: "INFP",
+        pros: ["True Idealists", "Motivated by Pure Intentions", "Curious", "Maximize the Potential of Others", "Deep Communicators"],
+        cons: ["Shy", "Neglect Day-to-Day Activities", "Impractical", "Everything is Personal", "Very Private"],
+        work: ["Keep the Data far Away", "High Level of Independence", "Face-to-Face Interactions", "More than Just a Job", "Down with Bureaucracy"],
+        snippets: ["Few personality types are as poetic and kind-hearted as INFPs."],
+        names: ["William Shakespeare", "Julia Roberts", "Homer", "Lisa Kudrow", "J.R.R. Tolkien"]
+      }, {
+        letters: "INTP",
+        pros: ["Theoretical", "Vigorous Intellect", "Unique Perspective", "Flexible", "Incredible Focus"],
+        cons: ["Extremely Private", "Absent-minded", "Can't Stand Rules", "Condescending", "Insensitive"],
+        work: ["They'll Do the Math", "Self-Motivating", "Perfectionism is the Standard", "Analysts", "People Skills Not a Priority"],
+        snippets: ["INTPs are the most logically precise of all the personality types."],
+        names: ["Albert Einstein", "Carl Jung", "Gerald Ford", "Isaac Newton", "Rene Descartes"]
+      }, {
+        letters: "ENFP",
+        pros: ["Spontaneous", "Comfortable in Unfamiliar Situations", "Energetic", "Pay Attention to Everything", "Brilliantly perceptive"],
+        cons: ["Attention Seekers", "Irrational", "Rule Breakers", "Prone to Flights of Fantasy", "Can Make Serious Mistakes in Judgment"],
+        work: ["Variety", "Full of Challenges", "Free from Tight Supervision", "Many Activities", "Creativity and Originality are Highly Valued"],
+        snippets: ["When people fall in love, they resemble garden-variety ENFPs because ENFPs normally behave like people in love."],
+        names: ["Dr. Suess", "Mark Twain", "Ellen Degeneres", "Walt Disney", "Sandra Bullock"]
+      }, {
+        letters: "ESTP",
+        pros: ["Flexible", "Tolerant", "Pragmatic", "Energetic", "Spontaneous"],
+        cons: ["Very Competetive", "Weaknesses are Failures", "Easily Offends Others", "Arrogant", "Too Impulsive"],
+        work: ["High Stress Levels Welcomed","Competition is Key","Social Intelligence","Throw Out the Rules","Immediate Results"],
+        snippets: ["ESTPs enjoy drama, passion, and pleasure."],
+        names: ["Ernest Hemingway", "Madonna", "Roy Rogers","Jack Nicholson", "Joan Cusack"]
+      }, {
+        letters: "ESFP",
+        pros: ["Outgoing", "Friendly", "Accepting", "Exuberant Lovers of Life", "Flexible"],
+        cons: ["Can be Difficult to Follow their Logic", "Not Very Loyal", "Will Talk Your Ears Off", "May Leave Unexpectedly"],
+        work: ["Work Directly with People", "Good in High Stress Positions","Freedom", "Novelty", "A Tad Chaotic"],
+        snippets: ["ESFPs love people, excitement, telling stories and having fun."],
+        names: ["Mary Lou Retton","Arsenio Hall","Bob Hope", "Eva Gabor","Kyle Petty"]
+      }, {
+        letters: "ENTP",
+        pros: ["Verbal Gymnasts", "Ingenious", "Stimulating", "Alert", "Outspoken"],
+        cons: ["Overextend Themselves", "Easily Affected by Small Setbacks","Impatient","Show Offs","Arrogant"],
+        work: ["Straightforward Communication","Big Challenges", "No Detail Work", "Prestige Positions", "Work with Similar Types"],
+        snippets: ["ENTPs may sometimes give the impression of being largely oblivious to the rest of humanity except as an audience"],
+        names: ["John Candy","Julia Child","Lewis Carrol","Thomas Edison", "John Adams"]
+      }, {
+        letters: "ESTJ",
+        pros: ["Practical", "Realistic", "Matter-of-Fact", "Decisive", "Detail-Oriented"],
+        cons: ["Judgmental of Those Outside the Norm","Narrow-minded", "Can Get Stuck in the Past", "Vocal Disapproval","Inflexible"],
+        work: ["Can Organize Large Groups", "Work Well with Similar Types", "Rules Are Respected","Service-Oriented","Well-Established"],
+        snippets: ["ESTJ humor is frequently centered around something or someone being off center or behaving abnormally."],
+        names: ["George W. Bush", "Bette Davis", "Rev. Billy Graham", "Pat Summit", "Barbara Stanwyck"]
+      }, {
+        letters: "ESFJ",
+        pros: ["Warmhearted", "Conscientious", "Cooperative", "Work Well with Others", "Dependable"],
+        cons: ["Easily Wounded","Conflicted", "No Gray Areas", "Paranoid", "Easily Angered"],
+        work: ["Clear Hierarchy", "Make Good Supervisors", "Low Conflict", "Oraganized", "Human Interaction"],
+        snippets: ["Other Types Coming Soon"],
+        names: ["Sally Field","Nancy Kerrigan", "Danny Glover","Mary Tyler Moore", "Don Knotts"]
+      }, {
+        letters: "ENFJ",
+        pros: ["Warm", "Empathetic", "Responsive", "Responsible", "See Potential in Everyone"],
+        cons: ["Can be Manipulative","Often Abused by Less Sensitive People", "Intimate Friendships are Rare", "Too Sacrificial", ],
+        work: ["Simultaneous Projects", "Startup Culture", "Fewer Coworkers", "Clear Separation of Duties", "Open Communication"],
+        snippets: ["ENFJs often take on more of the burdens of others than they can bear."],
+        names: ["Oprah Winfrey", "Dick Van Dyke","Ronald Reagan", "Diane Sawyer", "Lauren Graham"]
+      }, {
+        letters: "ENTJ",
+        pros: ["Frank", "Decisive", "Assume Leadership", "Problem Solvers", "Well-Informed"],
+        cons: ["Argumentative","Bossy", "Insensitive", "Intolerant", "Stubborn"],
+        work: ["Structure and Order", "High Standards", "Opportunities for Advancement", "New Challenges Available"],
+        snippets: ["The underlying thought running through the ENTJ mind might be something like 'I don't care if you call me an insensitive bastard, as long as I remain an efficient bastard"],
+        names: ["Newt Gingrich", "Steve Martin", "Steve Jobs", "Sigourney Weaver", "Whoopi Goldberg"]
+      } ];
+      self.chosenType = ko.observable();
+      self.chosenStrengths = ko.observable();
+      self.chosenWeaknesses = ko.observable();
+      self.chosenWork = ko.observable();
+      self.chosenFamous = ko.observable();
+      self.chosenQuote = ko.observable();
+      var headerAway = false;
 
 
 
-	// Behaviors
-	self.chooseType = function() {
-		$('.info').removeClass("hidden");
-		self.chosenType(this.letters);
-		self.chosenStrengths(this.pros);
-		self.chosenWeaknesses(this.cons);
-		self.chosenWork(this.work);
-		self.chosenFamous(this.names);
-		self.chosenQuote(this.snippets);
-		
-	};
-};
+      // Behaviors
 
-ko.applyBindings(new ViewModel());
+      self.removeHeader = function() {
+        $('#header').remove();
+        headerAway = true;
+        $('#theChosenType').prependTo('#topTypes').addClass('fixed');
+        $('#theTypes').prependTo('#topTypes').addClass('fixed');
+
+      };
+      self.chooseType = function() {
+        if (headerAway == false) {
+          self.removeHeader();
+        }
+        $('.info').removeClass("hidden");
+        $('#theChosenType').removeClass('hidden');
+        $('')
+        self.chosenType(this.letters);
+        self.chosenStrengths(this.pros);
+        self.chosenWeaknesses(this.cons);
+        self.chosenWork(this.work);
+        self.chosenFamous(this.names);
+        self.chosenQuote(this.snippets);
+      }
+
+    };
+
+
+    ko.applyBindings(new ViewModel());
