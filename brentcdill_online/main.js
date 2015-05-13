5eb25dc226d81edead8e1ac91683770fffec3d08
Brenton Dill <bdill12@gmail.com>
2015-05-13 05:51:29 -0700

    i heard

--- brentcdill_online/main.js
+++ brentcdill_online/main.js
@@ -93,9 +93,27 @@ function ViewAppModel() {
     self.webService(this.webServices[number].webService);
     self.webDesc(this.webServices[number].webDesc);
     self.webPrice(this.webServices[number].price);
+
+    $(".response").on("tap",function(e){
+      if (e.text == services) {
+        self.showServices();
+      } else if (e.text == "web projects") {
+        self.showProjects();
+      } else if (e.text == contact) {
+        self.showContact();
+      } else {
+        self.showAbout();
+      }
+      }
+    );
+
+
   };
+
 }
+
 ko.applyBindings(new ViewAppModel());
+
 
 /* If the screen is larger than 480px, then implement the scrolling and fixed menu */
 //scrolling
